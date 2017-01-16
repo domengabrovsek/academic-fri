51d498b6a496d834d6033f07dd18ac6406648f52
Charapanga <sk4266@student.uni-lj.si>
2017-01-16 03:34:04 +0100

    Dodan android project.

---
+++ AndroidStudioProject/ServiceChat/app/src/main/java/com/example/simon/servicechat/grabber.java
@@ -0,0 +1,8 @@
+package com.example.simon.servicechat;
+
+/**
+ * Created by Simon on 16. 01. 2017.
+ */
+
+public class grabber {
+}
---
+++ AndroidStudioProject/ServiceChat/app/src/main/java/com/example/simon/servicechat/httpHandler.java
@@ -0,0 +1,8 @@
+package com.example.simon.servicechat;
+
+/**
+ * Created by Simon on 15. 01. 2017.
+ */
+
+public class httpHandler {
+}
