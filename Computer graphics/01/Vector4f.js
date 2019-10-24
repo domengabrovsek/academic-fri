1fce466f65f1512b0870fc1e55905bda923591a3
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2019-10-24 13:32:17 +0200

    Fixed vectors logic

--- Computer graphics/01/Vector4f.js
+++ Computer graphics/01/Vector4f.js
@@ -2,69 +2,66 @@
 
 class Vector4f {
 
-    // constructor
-    constructor(x, y, z) {
+    constructor(x, y, z, w) {
         this.x = x;
         this.y = y;
         this.z = z;
+        this.w = 1; // homogena komponenta
     };
 
-    // methods
-
-    // negate input vector
+    // negacija vektorja
     static negate(vector) {
-        // check if input is of correct type and length
-        if(!(vector instanceof Vector4f && Object.keys(vector).length === 3)) {
-            return new Error('Vhod mora biti tipa Vector4f in more vsebovati koordinate x,y in z.')
+        // check za pravi tip in dolžino inputa
+        if(!(vector instanceof Vector4f && Object.keys(vector).length === 4)) {
+            return new Error('Vhod mora biti tipa Vector4f in more vsebovati koordinate x,y,z,w!')
         }
 
-        // multiply every coordinate by -1 to negate the vector
-        return new Vector4f(...Object.values(vector).map(x => x * -1));
+        // pomnoži vse komponente z -1
+        return new Vector4f(...Object.keys(vector).filter(x => x !== 'w').map(x => vector[x] * -1));
     };
 
-    // add two vectors
+    // seštevek dveh vektorjev
     static add(vector1, vector2) {
-
-        // there should be only two vectors in input, both of same type and length
-        if(!(Object.values(arguments).every(vector => vector instanceof Vector4f && Object.keys(vector).length === 3) && Object.values(arguments).length === 2)) {
-            return;
+        // check za pravi tip in dolžino inputa
+        if(!(Object.values(arguments).every(vector => vector instanceof Vector4f && Object.keys(vector).length === 4) && Object.values(arguments).length === 2)) {
+            return new Error('Vhod morata biti dva vektorja tipa Vector4f in morata vsebovati koordinate x,y,z!');
         }
 
-        // sum all elements at same indexes in both arrays
-        return new Vector4f(...Object.values(vector1).map((n, i) => n + Object.values(vector2)[i]));
+        // seštej vse istoležne elemente
+        return new Vector4f(...Object.keys(vector1).filter(x => x !== 'w').map((n, i) => vector1[n] + Object.values(vector2)[i]));
     };
 
-    // scalar product (scalar * vector)
+    // produkt s skalarjem
     static scalarProduct(scalar, vector) {
-        
+        // skalar mora biti številka
         if(typeof(scalar) !== 'number') {
-            return;
+            return new Error('Skalar mora biti številka!')
         }
-
-        if(!(vector instanceof Vector4f && Object.keys(vector).length === 3)) {
-            return;
+        // check za pravi tip in dolžino inputa
+        if(!(vector instanceof Vector4f && Object.keys(vector).length === 4)) {
+            return new Error('Podan vektor mora biti tipa Vector4f in mora vsebovati koordinate x,y,z,w');
         }
 
         return new Vector4f(...Object.values(vector).map(value => value * scalar));
-
     };
 
-    // dot product (vector * vector)
+    // skalarni produkt
     static dotProduct(vector1, vector2) {
-        // there should be only two vectors in input, both of same type and length
-        if(!(Object.values(arguments).every(vector => vector instanceof Vector4f && Object.keys(vector).length === 3) && Object.values(arguments).length === 2)) {
-            return;
+        // check za pravi tip in dolžino inputa
+        if(!(Object.values(arguments).every(vector => vector instanceof Vector4f && Object.keys(vector).length === 4) && Object.values(arguments).length === 2)) {
+            return new Error('Vhod morata biti dva vektorja tipa Vector4f in morata vsebovati koordinate x,y,z,w!');
         }
 
-        // multiply elements at same index and sum them up
-        return Object.values(vector1).map((n, i) => n * Object.values(vector2)[i]).reduce((sum, value) => sum + value, 0);
+        // zmnoži istoležne elemente in jih seštej
+        return Object.keys(vector1).filter(x => x !== 'w').map((n, i) => vector1[n] * Object.values(vector2)[i]).reduce((sum, value) => sum + value, 0);
 
     };
 
+    // vektorski produkt
     static crossProduct(vector1, vector2) {
-        // there should be only two vectors in input, both of same type and length
-        if(!(Object.values(arguments).every(vector => vector instanceof Vector4f && Object.keys(vector).length === 3) && Object.values(arguments).length === 2)) {
-            return;
+        // check za pravi tip in dolžino inputa
+        if(!(Object.values(arguments).every(vector => vector instanceof Vector4f && Object.keys(vector).length === 4) && Object.values(arguments).length === 2)) {
+            return new Error('Vhod morata biti dva vektorja tipa Vector4f in morata vsebovati koordinate x,y,z,w!');
         }
 
         const x = vector1.y * vector2.z - vector1.z * vector2.y;
@@ -72,88 +69,85 @@ class Vector4f {
         const z = vector1.x * vector2.y - vector1.y * vector2.x;
 
         return new Vector4f(x, y, z);
-
     };
 
-    // second norm -> square root of sum of all elements to the second power 
+    // dolžina vektorja = druga norma = kvadratni koren vsote vseh komponent na drugo potenco
     static length(vector) {
-        if(!(vector instanceof Vector4f) && Object.keys(vector).length === 3) {
-            return;
+        if(!(vector instanceof Vector4f) && Object.keys(vector).length === 4) {
+            return new Error('Vhod mora biti tipa Vector4f in more vsebovati koordinate x,y,z,w.')
         }
 
-        return Math.sqrt(Object.values(vector).reduce((length, value) => length + Math.pow(value, 2), 0));
+        return Math.sqrt(Object.keys(vector).filter(x => x !== 'w').reduce((length, key) => length + Math.pow(vector[key], 2), 0));
     };
 
-    // unit vector -> vector divided by its length
+    // normalizacija vektorja
     static normalize(vector) {
-        if(!(vector instanceof Vector4f) && Object.keys(vector).length === 3) {
-            return;
+        if(!(vector instanceof Vector4f) && Object.keys(vector).length === 4) {
+            return new Error('Vhod mora biti tipa Vector4f in more vsebovati koordinate x,y,z,w.')
         }
 
-        // we cannot normalize vector with length 0 because division with 0 is not defined
+        // vektorja z dolžino 0 ne moremo normalizirati
         if(this.length(vector) <= 0) {
-            return;
+            return new Error('vektorja z dolžino 0 ne moremo normalizirati!');
         }
 
         return new Vector4f(...Object.values(vector).map(value => value /= this.length(vector)));
     };
 
-    // scalar product (dot product(vector1, vector2) / vector1.length^2, vector1)
+    // produkt s skalarjem * (skalarni produkt(vektor, vektor2) / dolžina prvega vektorja, vektor1)
     static project(vector1, vector2) {
-        // there should be only two vectors in input, both of same type and length
-        if(!(Object.values(arguments).every(vector => vector instanceof Vector4f && Object.keys(vector).length === 3) && Object.values(arguments).length === 2)) {
-            return;
+        // check za pravi tip in dolžino inputa
+        if(!(Object.values(arguments).every(vector => vector instanceof Vector4f && Object.keys(vector).length === 4) && Object.values(arguments).length === 2)) {
+            return new Error('Vhod morata biti dva vektorja tipa Vector4f in morata vsebovati koordinate x,y,z,w!');
         }
 
         const dotProduct = this.dotProduct(vector1, vector2);
         const firstLength = this.length(vector1);
 
         if(firstLength <= 0) {
-            return;
+            return new Error('Vektorja z dolžino 0 ne moremo projecirati!');
         }
 
         const scalarProduct = this.scalarProduct(dotProduct / Math.pow(firstLength, 2), vector1);
 
-        return scalarProduct;
-        
+        return scalarProduct;    
     };
 
-    // vector lengths multiplied by cos phi is equal to dot product of vectors
+    // ||vector1|| * ||vector2|| * cos phi = skalarni produkt(vector1, vector2)
     static cosPhi(vector1, vector2) {
-        // there should be only two vectors in input, both of same type and length
-        if(!(Object.values(arguments).every(vector => vector instanceof Vector4f && Object.keys(vector).length === 3) && Object.values(arguments).length === 2)) {
-            return;
+        // check za pravi tip in dolžino inputa
+        if(!(Object.values(arguments).every(vector => vector instanceof Vector4f && Object.keys(vector).length === 4) && Object.values(arguments).length === 2)) {
+            return new Error('Vhod morata biti dva vektorja tipa Vector4f in morata vsebovati koordinate x,y,z,w!');
         }
 
         const dotProduct = this.dotProduct(vector1, vector2);
         const firstLength = this.length(vector1);
         const secondLength = this.length(vector2);
 
-        // division by zero protecion, if any of those is 0 then cos phi is 0
+        // preprečimo deljenje z 0
         if([dotProduct, firstLength, secondLength].includes(0)) {
             return 0;
         }
 
-        // dot product = vector1.length * vector2.length * cos phi
         return dotProduct / firstLength / secondLength;
-
     };
 }
 
 // test helpers
 
-const vector1 = new Vector4f(1, 2, 3);
-const vector2 = new Vector4f(3, 2, 1);
+// const vector1 = new Vector4f(1, 2, 3);
+// const vector2 = new Vector4f(3, 2, 1);
 
-console.log('Vektor 1: ', vector1);
-console.log('Vektor 2: ', vector2);
+// console.clear();
+// console.log('Vektor 1: ', vector1);
+// console.log('Vektor 2: ', vector2);
 
-console.log('Negate: ', Vector4f.negate(vector1));
-console.log('Add: ', Vector4f.add(vector1, vector2));
-console.log('Scalar product: ', Vector4f.scalarProduct(5, vector1));
-console.log('Dot product: ', Vector4f.dotProduct(vector1, vector2));
-console.log('Cross product: ', Vector4f.crossProduct(vector1, vector2));
-console.log('Length: ', Vector4f.length(vector1));
-console.log('Normalize: ', Vector4f.normalize(vector1));
-console.log('Project: ', Vector4f.project(vector1, vector2));
-console.log('Cos phi: ', Vector4f.cosPhi(vector1, vector2));
+// console.log('Negate: ', Vector4f.negate(vector1));
+// console.log('Add: ', Vector4f.add(vector1, vector2));
+// console.log('Scalar product: ', Vector4f.scalarProduct(5, vector1));
+// console.log('Dot product: ', Vector4f.dotProduct(vector1, vector2));
+// console.log('Cross product: ', Vector4f.crossProduct(vector1, vector2));
+// console.log('Length: ', Vector4f.length(vector1));
+// console.log('Normalize: ', Vector4f.normalize(vector1));
+// console.log('Project: ', Vector4f.project(vector1, vector2));
+// console.log('Cos phi: ', Vector4f.cosPhi(vector1, vector2));
