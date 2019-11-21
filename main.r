92ec7a22056a94c2f17576cbfe99ebfefba00ac7
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2019-11-21 20:42:12 +0100

    Added libraries to main.r

--- main.r
+++ main.r
@@ -3,8 +3,21 @@
 # predvidevamo da imamo nastavljen "working directory" in da so vse potrebne datoteke v isti mapi
 # oz uporabimo setwd(pot)
 
+# nalozimo knjiznice, ki jih bomo potrebovali
+install.packages(c("ipred", "prodlim", "CORElearn", "e1071", "randomForest", "kernlab", "nnet"))
+
+# nalozimo knjiznico za precno preverjanje
+library(ipred)
+
+# nalozimo knjiznico za grajenje odlocitvenih dreves
+library(rpart)
+
+# nalozimo knjiznico ki podpira grajenje razlicnih modelov
+library(CORElearn)
+
 # nalozimo svoje custom funkcije
 source("funkcije.r")
 
 # nalozimo dataset
 data <- read.table("podatkiSem1.txt", header = T, sep = ",")
+
