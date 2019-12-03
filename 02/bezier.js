a262993f36c85acf2d9a728ce821f065b585a4c6
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2019-12-03 09:54:36 +0100

    Added functions for drawing circle and square

--- 02/bezier.js
+++ 02/bezier.js
@@ -49,15 +49,53 @@ function init () {
 
     // mousedown
     canvas.addEventListener('mousedown', e => {
-        context.beginPath();
-        context.moveTo(e.clientX, e.clientY);
-        console.log(`Starting point: x:${e.clientX}, y:${e.clientY}`);
+        // context.beginPath();
+        // context.moveTo(e.clientX, e.clientY);
+        // console.log(`Starting point: x:${e.clientX}, y:${e.clientY}`);
     });
 
     // mouseup
     canvas.addEventListener('mouseup', e => {
-        context.lineTo(e.clientX, e.clientY);
-        context.stroke();
-        console.log(`Ending point: x:${e.clientX}, y:${e.clientY}`);
+        // context.lineTo(e.clientX, e.clientY);
+        // context.stroke();
+        // console.log(`Ending point: x:${e.clientX}, y:${e.clientY}`);
+    });
+
+    canvas.addEventListener('click', e => {
+        drawCircle({ context, x: e.clientX, y: e.clientY, size: 3, fillColor: 'orange', borderColor: 'black'});
+        // drawSquare({ context, x: e.clientX + 50, y: e.clientY + 50, size: 8, fillColor: 'black', borderColor: 'black' });
     });
 }
+
+function drawSquare(properties) {
+    const { x, y, size, fillColor, borderColor, context } = properties;
+    context.fillStyle = fillColor;
+    context.strokeStyle = borderColor;
+    context.fillRect(x -10, y - 10, size, size);
+}
+
+function drawCircle(properties) {
+    const { x, y, size, fillColor, borderColor, context } = properties;
+    context.beginPath();
+    context.arc(x - 10, y - 10, size, 0, 2 * Math.PI, false);
+    context.fillStyle = fillColor;
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = borderColor;
+    context.stroke();
+}
+
+function bezier(t, p0, p1, p2, p3){
+    var cX = 3 * (p1.x - p0.x),
+        bX = 3 * (p2.x - p1.x) - cX,
+        aX = p3.x - p0.x - cX - bX;
+  
+    var cY = 3 * (p1.y - p0.y),
+        bY = 3 * (p2.y - p1.y) - cY,
+        aY = p3.y - p0.y - cY - bY;
+  
+    var x = (aX * Math.pow(t, 3)) + (bX * Math.pow(t, 2)) + (cX * t) + p0.x;
+    var y = (aY * Math.pow(t, 3)) + (bY * Math.pow(t, 2)) + (cY * t) + p0.y;
+  
+    return {x: x, y: y};
+  }
