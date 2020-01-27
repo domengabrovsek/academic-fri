604ece0e2866e1769a6e5a7984bb770d6787e719
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2020-01-27 03:17:13 +0100

    Finished with dn 02

--- computer-organization/DN_02/program.s
+++ computer-organization/DN_02/program.s
@@ -42,7 +42,7 @@
 
 .text
 
-# zacetni naslov
+# morsejeva abeceda
 morse_code_address:
   .ascii ".-"     @ A
   .byte 0,0,0,0
@@ -97,7 +97,7 @@ morse_code_address:
   .ascii "--.."   @ Z
   .byte 0,0
 
-source:   .space 100
+letters:   .space 100
 
 .align
 .code 32
@@ -177,20 +177,47 @@ bl INIT_TC0
 bl INIT_DBGU
 
 # glavna zanka v programu
+MAIN_LOOP:
+  adr r0, letters
+  bl RECEIVE
+  bl XWORD
 
-main_loop:
-  @ adr r0, source
-  @ bl receive
-  @ bl xword
+  /*  
+    
+  test algoritma za GETMCODE
 
-  bl LED_ON
-  mov r0, #500
-  bl DELAY
-  bl LED_OFF
-  mov r0, #500
-  bl DELAY
+  adr r1, morse_code_address @ 32 (0x20)
 
-  b main_loop
+  # v register r0 shranimo konstanto 'E'
+  mov r0, #'E' @ 69 (0x45)
+
+  # v register r2 shranimo konstanto 'A'
+  mov r2, #'A' @ 65 (0x41)
+
+  # v register r4 shranimo konstanto 6, za mnozenje
+  mov r4, #6
+
+  sub r3, r0, r2 @ 69 - 65 = 4
+  mul r0, r3, r4 @ 4 * 6 = 24 (0x18)
+  add r0, r0, r1 @ 32 (0x20) + 24 (0x18) = 56 (0x38)
+
+  # ascii vrednosti crk
+  # A 41
+  # B 42
+  # C 43
+  # D 44
+  # E 45
+
+  # naslovi crk decimalno
+  # A 32-37
+  # B 38-43
+  # C 44-49
+  # D 50-55
+  # E 56-62
+
+  */  
+
+  b MAIN_LOOP
 
 # konec glavnega programa
 
@@ -224,32 +251,35 @@ INIT_IO:
   # vrnitev iz podprograma
   ldmfd sp!, {r0-r12, pc}
 
-# inicializacija za TC0
+# inicializacija za TC0 casovnik
 INIT_TC0:
   # shranimo na sklad vse registre r0-r3
-  stmfd sp!, {r0-r3,lr}
+  stmfd sp!, {r0-r3, lr}
 
-  # bazni register
+  # prizgemo TMC za TC0
   ldr r0, =PMC_BASE
 
   # omogocimo urin signal za TC0 z vpisom 1 v bit 17 v register PMC_PCER
   mov r1, #0b1 << 17
   str r1, [r0, #PMC_PCER]
-  ldr r0,=TC0_BASE
 
-  # nastavi WAVE nacin (generator signalov), WAVE=1, WAVESEL=10
-  mov r1,#0b110 << 13
+  # bazni register za TC0
+  # MCK/128, RC=375 (1ms)
+  ldr r0, =TC0_BASE
+
+  # nastavimo WAVE nacin (generator signalov), WAVE=1, WAVESEL=10
+  mov r1, #0b110 << 13
   
-  # izbira frekvence stetja, MCK = 48 MHz, inicializiraj TC0 s MCK/128
+  # izbira frekvence stetja, MCK = 48 MHz, inicializiramo TC0 s MCK/128
   mov r2, #0b011
   add r1, r1, r2
   str r1, [r0, #TC_CMR]
   
-  # 1ms ==> RC=375
+  # RC=375 (1ms)
   ldr r1, =375
-  str r1, [r0,#TC_RC]
+  str r1, [r0, #TC_RC]
   
-  # omogoci in sprozi uro z vpisom
+  # omogocimo in sprozimo uro z vpisom
   # 1 v CLKEN (bit 0 v TC_CCRx)  
   # 1 v SWTRG (bit 2 v TC_CCRx) 
   mov r1, #0b101
@@ -259,25 +289,31 @@ INIT_TC0:
   ldmfd sp!, {r0-r3, pc}
 
 # inicializacija za DBGU
+# 1. nastavi nacin delovanja (normal mode) in parnost z vpisom v DBGR_MR
+# 2. nastavi hitrost prenosa z vpisom v DBGU_BRGR, formula: MCK[Hz] / (16 * BAUD_RATE)
+# 3. omogoci oddajnik/sprejemnik z vpisom 1 v bita TXEN/RXEN v DBGU_CR
 INIT_DBGU:
-  # shranimo na sklad vse registre r0-r2
+  # shranimo na sklad vse registre r0, r1, r2
   stmfd sp!, {r0-r2, lr} 
 
-  # bazni register za DBGU napravo
+  # v register r0 shranimo naslov baznega registra za DBGU
   ldr r0, =DBGU_BASE
   
-  # nastavi normal mode in brez paritete v DBGU_MR
+  # nastavimo normal mode in brez paritete v DBGU_MR
   ldr r1, =1 << 11
   str r1, [r0, #DBGU_MR]
   
-  # nastavi hitrost prenosa z vpisom v BRGR nastavi na 9600 boudov (CD=9600)
-  # izracuaj po formuli MCK=48MHz, MCK(Hz)/(16*CD) = 9600 boudov
-  mov r1,#312
-  str r1,[r0,#DBGU_BRGR]
+  # nastavimo hitrost prenosa z vpisom v BRGR 
+  # mov r1, #26  @ BR = 115200
+  # mov r1, #156 @ BR = 19200
+  # mov r1, #312 @ BR = 9600
+  mov r1, #312
+  str r1, [r0, #DBGU_BRGR]
   
-  # omogoci sprejemnik z vpisom 1 v bit RXEN
-  # v registru DBGU_CR (za oba)
-  mov r1, #1 << 4   @samo RXEN(b4)
+  # omogocimo sprejemnik z vpisom 1 v bit 4 v registru DBGU_CR 
+  # RXEN (oddajnik) - bit 4
+  # TXEN (sprejemnik) - bit 6 (ne potrebujemo)
+  mov r1, #1 << 4
   str r1, [r0, #DBGU_CR]  
   
   # vrnitev iz podprograma
@@ -307,7 +343,7 @@ LED_OFF:
   # bazni register za PIO
   ldr r0, =PIOC_BASE
 
-  # TODO komentarji
+  # ugasnemo led diodo
   mov r1, #2      
   str r1, [r0, #PIO_SODR]
 
@@ -318,116 +354,242 @@ LED_OFF:
 # preko r0 sprejmemo parameter za dolzino zakasnitve
 DELAY:
   # shranimo na sklad vse registre r1-r12
-  stmfd sp!, {r1-r12, lr}
+  stmfd sp!, {r0, r2, r3, lr}
 
-  # zanka za zakasnitev
-  ldr r2, loop_delay
-  loop1:
-    mov r1, r2
-    loop:
-      subs r1, #1
-      bne loop
-    subs r0, #1
-    bne loop1
+  # v register r0 shranimo bazni register od casovnika
+  ldr r0, = TC0_BASE
+
+  # zastavica CPCS
+  mov r2, #1 << 4
+
+DELAY_LOOP:
+  ldr r3, [r0, #TC_SR]
+
+  # preveri zastavico CPCS v TC_SR
+  tst r3, r2
+
+  beq DELAY_LOOP
+
+  # pocakamo kolikor milisekund je podano v registru r1
+  subs r1, r1, #1
+  bne DELAY_LOOP
   
   # vrnitev iz podprograma
-  ldmfd sp!, {r1-r12, pc}
+  ldmfd sp!, {r0, r2, r3, pc}
   
 # prek r0 prejme kazalec na zacetek prostora za shranjevanje podatkov 
 RECEIVE:
-  # shranimo na sklad vse registre r1-r3
+  # shranimo na sklad vse registre r0, r1, r2, r3
   stmfd sp!, {r0-r3, lr}
-  mov r1,#0 @ odmik
 
-read:
+  # odmik, ki ga bomo uporabili za shranjevanje znakov
+  mov r1, #0 
+
+READ:
+  # sprejem znaka preko DBGU
+  # znak dobimo v register r2
   bl RCV_DBGU
-  @ prek r2 dobi znak
-  strb r2,[r0,r1] @shrani znak v rez. prostor
-  cmp r2,#13
-  beq end_read
-  add r1,r1,#1
-  b read
   
-end_read:
-  mov r2,#0 @ znak carriage return (ascii=13) nadomsti z 0
-  strb r2,[r0,r1]
-  ldmfd sp!,{r0-r3,pc}
+  # shrani znak v rezerviran prostor
+  strb r2, [r0, r1] 
 
+  # preveri ce je znak carriage return (enter, ascii = 13)
+  cmp r2, #13
+
+  # ce je enter, koncamo branje
+  beq END_READ
+
+  # ce ni enter, povecamo odmik za 1 ...
+  add r1, r1, #1
+
+  # ...in nadaljujemo z branjem
+  b READ
+  
+END_READ:
+  # v registru r2 imamo prebran znak (enter, ascii = 13)
+  # ker mora biti niz koncan z 0, 13 zamenjamo z 0
+  mov r2, #0 
+
+  # zapisemo vrednost v pomnilnik, v registru r1 hranimo odmik, 
+  # katerega povecujemo ob vsakem prebranem znaku (razen enter)
+  strb r2, [r0, r1]
+
+  # vrnitev iz podprograma
+  ldmfd sp!, {r0-r3, pc}
+
+# sprejem znaka preko DBGU
 RCV_DBGU:
-  stmfd sp!,{r0-r1,lr}
-  ldr r0, =DBGU_BASE
-  mov r1, #1 @ bit 0 (RXRDY) 
+  # shranimo na sklad registra r0 in r1
+  stmfd sp!, {r0-r1, lr}
 
-check_receive_ready:
-  ldr r2,[r0,#DBGU_SR]
-  tst r2,r1
-  beq check_receive_ready
+  # v r0 shranimo naslov baznega registra za DBGU
+  ldr r0, =DBGU_BASE
+
+RCVD_LP:
+
+  # ob sprejemu znaka se postavi bit 0 (RXRDY) v DBGU_SR, preberemo ga v register r2
+  ldr r2, [r0, #DBGU_SR]
+
+  # pogledamo ce je bit RXRDY postavljen
+  tst r2, #1
+
+  # ce ni postavljen, cakamo
+  beq RCVD_LP
+
+  # ce je postavljen, preberemo znak iz spodnjih 8 bitov v DBGU_RHR
   ldr r2, [r0, #DBGU_RHR]   
+
+  # vrnitev iz podprograma, v registru r2 je prebran znak
   ldmfd sp!, {r0-r1, pc}
 
-# dobi parameter v registru r0 (parameter je lahko . ali -)
-# glede na parameter odda signal ustrezne dolzine
+# v registru r0 sprejmemo znak "." ali "-"
+# glede na poslan znak ustrezno prizgemo/ugasnemo led diodo
 XMCHAR:
-  stmfd sp!,{r1,lr}
-  cmp r0,#'.'
-  moveq r1,#150
-  movne r1,#300
-  bl LED_ON
-  bl DELAY
-  mov r1,#150
-  bl LED_OFF
-  bl DELAY
-  ldmfd sp!,{r1,pc}
 
-# v r0 dobi kazalec z 0 zakljucen niz ki vsebuje samo znake '.' ali '-'
-# poslje znak po znak podprogramu XMCHAR 
+  # shranimo na sklad register r1
+  stmfd sp!, {r1, lr}
+
+  # preverimo ce je poslan znak "."
+  cmp r0, #'.'
+
+  # ce je poslan znak "." nastavimo dolzino zakasnitve na 150ms
+  moveq r1, #150
+
+  # zanasamo se na to, da smo res poslali samo "." oz "-"
+  # ce je poslan znak "-" nastavimo dolzino zakasnitve na 300ms
+  movne r1, #300
+
+  # prizgemo led diodo
+  bl LED_ON
+
+  # pocakamo (zakasnitev)
+  bl DELAY
+
+  # ugasnemo led diodo
+  bl LED_OFF
+
+  # nastavimo zakasnitev na 150ms
+  mov r1, #150
+
+  # pocakamo (zakasnitev)
+  bl DELAY
+
+  # vrnitev iz podprograma
+  ldmfd sp!, {r1, pc}
+
+# v registru r0 dobimo kazalec na niz iz tabele morsejeve abecede,
+# ki vsebuje znake "." ali "-"
+# beremo znak po znak in klicemo podprogram XMCHAR
 XMCODE:
-  stmfd sp!,{r0-r2,lr}
-  mov r1,r0
-loop_xmcode:
-  ldrb r0,[r1],#1
-  cmp r0,#0
-  beq  END_XMCODE
-  bl  XMCHAR
-  b loop_xmcode
+  # shranimo na sklad registre r0, r1, r2
+  stmfd sp!, {r0-r2, lr}
+
+  # v register r1 si shranimo kazalec na niz
+  mov r1, r0
+
+# beremo znak po znak
+XMCODE_LOOP:
+
+  # preberemo niz in ga shranimo v register r0
+  ldrb r0, [r1], #1
+
+  # preverimo ce je prebran znak 0
+  cmp r0, #0
+
+  # ce je prebran znak 0 skocimo na END_XMCODE
+  beq END_XMCODE
+
+  # ce je prebran katerikoli drug znak skocimo na XMCHAR
+  bl XMCHAR
+
+  # beremo dalje
+  b XMCODE_LOOP
 
 END_XMCODE:
-  mov r1,#150
+  # v register r1 zapisemo konstanto 300 (koliko milisekund bo trajala zakasnitev)
+  mov r1, #300
+
+  # ugasnemo led diodo
   bl LED_OFF
+
+  # pocakamo 300 milisekund
   bl DELAY
+
+  # vrnitev iz podprograma
   ldmfd sp!, {r0-r2, pc}
 
-# v r0 dobi ascii znak velike tiskane crke 'A'-'Z' 
-# V r0 nato dobi kazalec na zacetek niza, kjer se nahaja ustrezno zaporedje znakov v Morsejevi abecedi
-GETMCODE:  
-  stmfd sp!,{r1-r5,lr}
-  adr r1, morse_code_address  @ zacetni naslov
+# v registru r0 sprejmemo ascii znak velike tiskane crke (A-Z, 65-90)
+# izracunamo naslov podanega znaka v tabeli morsejeve abecede
+# vrnemo kazalec na niz morsejeve abecede, ki ustreza crki v registru r0
+# (bolj podroben opis algoritma je podan na zacetku programa)
+GETMCODE:
+  # shranimo na sklad registra r1, r2, r3, r4
+  stmfd sp!, {r1-r4, lr}
+
+  # v register r1 preberemo zacetni naslov tabele, ki vsebuje morsejevo abecedo
+  adr r1, morse_code_address 
+
+  # v register r2 shranimo konstanto 'A'
   mov r2, #'A'
-  sub r3, r0, r2
-  mov r4, r3, lsl #2
-  mov r5, r3, lsl #1
-  add r0, r5, r4
-  add r0, r0, r1
+
+  # v register r3 si shranimo konstanto 6, ki jo potrebujemo za
+  # racunanje naslovov v tabeli morsejeve abecede
+  mov r3, #6
+
+  # odstejemo 41 od prebranega znaka in shranimo v register r4
+  sub r4, r0, r2
+
+  # mnozimo razliko, ki smo jo dobili v prejsnjem koraku z konstanto 6
+  mul r4, r4, r3
+
+  # sestejemo zacetni naslov tabele in produkt iz prejsnjega koraka
+  # da dobimo naslov trenutnega znaka v tabeli, katerega zapisemo v register r0
+  add r0, r4, r1
+
+  # poklicemo podprogram XMCODE
   bl XMCODE
-  ldmfd sp!, {r1-r5, pc}
 
-# v R0 dobi kazalec na niz velikih crk zakljucen z 0
+  # vrnitev iz podprograma
+  ldmfd sp!, {r1-r4, pc}
+
+# v registru r0 sprejmemo kazalec na niz velikih crk zakljucen z 0
+# klicemo GETMCODE crko po crko
+# ko naletimo na 0, pocakamo 1s in koncamo
 XWORD:
-  stmfd sp!,{r0-r1,lr}
-  mov r1,r0
-loop_xword:
-  ldrb r0,[r1],#1 
-  cmp r0,#0
-  beq end_xword
-  bl GETMCODE
-  b loop_xword 
-  
-end_xword:    
-  ldmfd sp!,{r0-r1,pc}
+  # shranimo na sklad registra r0, r1
+  stmfd sp!, {r0-r1, lr}
 
-/* konstante */
-loop_delay: .word 48000
-delay_ms: .word 100
+  # v r1 si shranimo kazalec na niz
+  mov r1, r0
+
+# beremo crko po crko dokler ne naletimo na konec niza (znak 0)
+XWORD_LOOP:
+
+  # v register r0 preberemo crko
+  ldrb r0, [r1], #1 
+
+  # preverimo ce smo prebrali znak 0
+  cmp r0, #0
+
+  # ce smo prebrali znak 0 koncamo
+  beq XWORD_END
+
+  # ce smo prebrali katerikoli drug znak, moramo najti ustrezno morsejevo kodo v tabeli
+  bl GETMCODE
+
+  # beremo dalje
+  b XWORD_LOOP 
+  
+# koncamo z branjem
+XWORD_END:   
+  # nastavimo zakasnitev na 1s (1000ms)
+  mov r1, #1000
+
+  # pocakamo (zakasnitev)
+  bl DELAY
+
+  # vrnitev iz podprograma
+  ldmfd sp!, {r0-r1, pc}
 
 _Lstack_end:
   .long __STACK_END__
